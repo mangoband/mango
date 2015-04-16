e8d18f6862561101c4774678edb45c2902dff8d4
mangoband <git@marcusjohansson.net>
2015-04-16 12:37:50 +0200

    changed in testfile

--- test/Flash/CFlashTest.php
+++ test/Flash/CFlashTest.php
@@ -23,9 +23,10 @@ class CFlashTest extends \PHPUnit_Framework_TestCase {
     public function testNullNotice(){
         
         $m = new CFlash();
-        $expected = $m->set($this->msg, 'notice' );
+        $expected = $m->set($this->msg, '' );
         $r = $m->get('notice');
-        $this->assertNull($expected);    
+        $this->assertNull($expected);
+        $this->assertStringEndsWith($this->textEnd, $r);
     }
     
     /**
@@ -36,7 +37,8 @@ class CFlashTest extends \PHPUnit_Framework_TestCase {
         $m = new CFlash();
         $expected = $m->set($this->msg, 'wrong' );
         $r = $m->get('wrong');
-         $this->assertNull($expected);    
+         $this->assertNull($expected);
+         $this->assertNull($r);
     }
     
     
